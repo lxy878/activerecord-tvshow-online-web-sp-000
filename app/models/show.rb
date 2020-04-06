0070af75304039f9626b5aa93f41bae57fc547e7
Xiaoyi Liu <lxy878@gmail.com>
2020-04-06 10:14:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,25 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.highest_rating.first
   end
+
+  def self.lowest_rating
+
+  end
+
+  def self.least_popular_show
+
+  end
+
+  def self.ratings_sum
+
+  end
+
+  def self.popular_shows
+
+  end
+
+  def self.shows_by_alphabetical_order
+
+  end
+  
 end
