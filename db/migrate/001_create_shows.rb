1005c21f771617341a36650d8366457ad2bf343d
Xiaoyi Liu <lxy878@gmail.com>
2020-04-06 09:48:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,9 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
-      :name
-      :network
-      :day
+      t.string :name
+      t.string :network
+      t.string :day
       t.integer :rating
     end
   end
